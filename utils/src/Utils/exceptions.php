000167ec9944c9a99fde26b375552c93c5b14243
David Grudl <david@grudl.com>
2008-04-21 11:14:41 +0000

    [utils] typos
    nette/utils@710207a

--- utils/src/Utils/exceptions.php
+++ utils/src/Utils/exceptions.php
@@ -1,50 +1,118 @@
 <?php
 
 /**
- * This file is part of the Nette Framework (https://nette.org)
- * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ * Nette Framework
+ *
+ * Copyright (c) 2004, 2008 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license" that is bundled
+ * with this package in the file license.txt.
+ *
+ * For more information please see http://nettephp.com/
+ *
+ * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @license    http://nettephp.com/license  Nette license
+ * @link       http://nettephp.com/
+ * @category   Nette
+ * @package    Nette
  */
 
-declare(strict_types=1);
+// no namespace
 
-namespace Nette\Utils;
+
+
+/*
+some useful SPL exception:
+
+- LogicException
+   - InvalidArgumentException
+   - LengthException
+- RuntimeException
+   - OutOfBoundsException
+   - UnexpectedValueException
+
+other SPL exceptions are ambiguous; do not use them
+*/
+
 
 
 /**
- * The exception that is thrown when an image error occurs.
+ * The exception that is thrown when the value of an argument is
+ * outside the allowable range of values as defined by the invoked method.
+ * @package    Nette
  */
-class ImageException extends \Exception
+class ArgumentOutOfRangeException extends InvalidArgumentException
 {
 }
 
 
+
 /**
- * The exception that indicates invalid image file.
+ * The exception that is thrown when a method call is invalid for the object's
+ * current state, method has been invoked at an illegal or inappropriate time.
+ * @package    Nette
  */
-class UnknownImageFileException extends ImageException
+class InvalidStateException extends RuntimeException // or InvalidOperationException?
 {
 }
 
 
+
 /**
- * The exception that indicates error of JSON encoding/decoding.
+ * The exception that is thrown when a requested method or operation is not implemented.
+ * @package    Nette
  */
-class JsonException extends \JsonException
+class NotImplementedException extends LogicException
 {
 }
 
 
+
 /**
- * The exception that indicates error of the last Regexp execution.
+ * The exception that is thrown when an invoked method is not supported. For scenarios where
+ * it is sometimes possible to perform the requested operation, see InvalidStateException.
+ * @package    Nette
  */
-class RegexpException extends \Exception
+class NotSupportedException extends LogicException
 {
 }
 
 
+
 /**
- * The exception that indicates assertion error.
+ * The exception that is thrown when accessing a class member (property or method) fails.
+ * @package    Nette
  */
-class AssertionException extends \Exception
+class MemberAccessException extends LogicException
+{
+}
+
+
+
+/**
+ * The exception that is thrown when an I/O error occurs.
+ * @package    Nette
+ */
+class IOException extends RuntimeException
+{
+}
+
+
+
+/**
+ * The exception that is thrown when accessing a file that does not exist on disk.
+ * @package    Nette
+ */
+class FileNotFoundException extends IOException
+{
+}
+
+
+
+/**
+ * The exception that is thrown when part of a file or directory cannot be found.
+ * @package    Nette
+ */
+class DirectoryNotFoundException extends IOException
 {
 }
