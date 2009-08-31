04750d69252280a25053062098628a8988955444
David Grudl <david@grudl.com>
2009-09-01 01:48:09 +0200

    [tester] Nette\Test: added new simple unit test framework
    nette/tester@0961f6e

--- tester/tests/Framework/fixtures/dataprovider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	0 => ['dataset-0'], // integer key
-	1 => ['dataset-1'], // integer key
-	'foo' => ['dataset-foo'],
-	'bar 1' => ['dataset-bar-1'],
-	'bar 2' => ['dataset-bar-2'],
-];
