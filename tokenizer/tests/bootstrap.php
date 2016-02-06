101494d6e7da0b0106ba5e51bf7533d946bb99ab
David Grudl <david@grudl.com>
2016-02-06 22:55:37 +0100

    [tokenizer] typo

--- tokenizer/tests/bootstrap.php
+++ tokenizer/tests/bootstrap.php
@@ -4,7 +4,7 @@
 // invoked through the command: ../vendor/bin/tester .
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
-	echo 'Install Nette Tester using `composer install`';
+	echo 'Install Nette Tester using `composer update --dev`';
 	exit(1);
 }
 
