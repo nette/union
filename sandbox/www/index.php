14661e0ad3959c1e5688b17af8f908cc67817937
Jáchym Toušek <enumag@gmail.com>
2019-06-04 20:51:00 +0200

    [sandbox] composer: removed platform (#100]

--- sandbox/www/index.php
+++ sandbox/www/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = App\Bootstrap::boot();
-$container = $configurator->createContainer();
-$application = $container->getByType(Nette\Application\Application::class);
-$application->run();
+App\Bootstrap::boot()
+	->createContainer()
+	->getByType(Nette\Application\Application::class)
+	->run();
