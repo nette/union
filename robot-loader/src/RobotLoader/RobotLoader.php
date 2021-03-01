162319cafb992f9d088545fbbbfd9162c6ec1ba2
David Grudl <david@grudl.com>
2021-03-01 19:38:04 +0100

    [robot-loader] added PHP 8 typehints

--- robot-loader/src/RobotLoader/RobotLoader.php
+++ robot-loader/src/RobotLoader/RobotLoader.php
@@ -46,17 +46,17 @@ class RobotLoader
 	/** @var string[] */
 	private array $excludeDirs = [];
 
-	/** @var array of class => [file, time] */
+	/** class => [file, time] */
 	private array $classes = [];
 
 	private bool $cacheLoaded = false;
 
 	private bool $refreshed = false;
 
-	/** @var array of missing classes */
+	/** missing classes */
 	private array $missing = [];
 
-	private ?string $tempDirectory;
+	private ?string $tempDirectory = null;
 
 
 	public function __construct()
@@ -70,7 +70,7 @@ class RobotLoader
 	/**
 	 * Register autoloader.
 	 */
-	public function register(bool $prepend = false): self
+	public function register(bool $prepend = false): static
 	{
 		spl_autoload_register([$this, 'tryLoad'], true, $prepend);
 		return $this;
@@ -116,16 +116,15 @@ class RobotLoader
 
 	/**
 	 * Add path or paths to list.
-	 * @param  string  ...$paths  absolute path
 	 */
-	public function addDirectory(...$paths): self
+	public function addDirectory(string ...$paths): static
 	{
 		$this->scanPaths = array_merge($this->scanPaths, $paths);
 		return $this;
 	}
 
 
-	public function reportParseErrors(bool $on = true): self
+	public function reportParseErrors(bool $on = true): static
 	{
 		$this->reportParseErrors = $on;
 		return $this;
@@ -134,9 +133,8 @@ class RobotLoader
 
 	/**
 	 * Excludes path or paths from list.
-	 * @param  string  ...$paths  absolute path
 	 */
-	public function excludeDirectory(...$paths): self
+	public function excludeDirectory(string ...$paths): static
 	{
 		$this->excludeDirs = array_merge($this->excludeDirs, $paths);
 		return $this;
@@ -375,7 +373,7 @@ class RobotLoader
 	/**
 	 * Sets auto-refresh mode.
 	 */
-	public function setAutoRefresh(bool $on = true): self
+	public function setAutoRefresh(bool $on = true): static
 	{
 		$this->autoRebuild = $on;
 		return $this;
@@ -385,7 +383,7 @@ class RobotLoader
 	/**
 	 * Sets path to temporary directory.
 	 */
-	public function setTempDirectory(string $dir): self
+	public function setTempDirectory(string $dir): static
 	{
 		Nette\Utils\FileSystem::createDir($dir);
 		$this->tempDirectory = $dir;
