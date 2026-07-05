a0c464b277360778a133d9ed1b4047f18702aba8
David Grudl <david@grudl.com>
2026-07-05 23:39:57 +0200

    [di] ServiceDefinition: creator can be any Expression
    enables first-class callable in 'create:'
    
    nette/di@9c49284

--- di/src/DI/Definitions/FactoryDefinition.php
+++ di/src/DI/Definitions/FactoryDefinition.php
@@ -120,12 +120,14 @@ final class FactoryDefinition extends Definition
 
 		if ($resultDef instanceof ServiceDefinition) {
 			$this->completeParameters($resolver);
-			$this->convertArguments($resultDef->getCreator()->arguments);
+			$creator = $resultDef->getCreator();
+			assert($creator instanceof Statement);
+			$this->convertArguments($creator->arguments);
 			foreach ($resultDef->getSetup() as $setup) {
 				$this->convertArguments($setup->arguments);
 			}
 
-			if ($resultDef->getEntity() instanceof Reference && !$resultDef->getCreator()->arguments) {
+			if ($resultDef->getEntity() instanceof Reference && !$creator->arguments) {
 				$resultDef->setCreator([ // render as $container->createMethod()
 					new Reference(Nette\DI\ContainerBuilder::ThisContainer),
 					Nette\DI\Container::getMethodName($resultDef->getEntity()->getValue()),
@@ -165,7 +167,7 @@ final class FactoryDefinition extends Definition
 					));
 				}
 
-				$this->resultDefinition->getCreator()->arguments[$ctorParam->getPosition()] = new Php\Literal('$' . $ctorParam->name);
+				$this->resultDefinition->setArgument($ctorParam->getPosition(), new Php\Literal('$' . $ctorParam->name));
 
 			} elseif (!$this->resultDefinition->getSetup()) {
 				// [param1, param2] => '$param1, $param2'
