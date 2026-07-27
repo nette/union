46c6a77e6d1429db31082ee9b2e838c35e6039bf
David Grudl <david@grudl.com>
2026-07-27 16:10:22 +0200

    [latte] Released version 3.1.6
    nette/latte@adccfac

--- latte/src/Latte/Engine.php
+++ latte/src/Latte/Engine.php
@@ -16,8 +16,8 @@ use function array_map, array_merge, class_exists, extension_loaded, get_debug_t
  */
 class Engine
 {
-	public const Version = '3.1.5';
-	public const VersionId = 30105;
+	public const Version = '3.1.6';
+	public const VersionId = 30106;
 
 	/** @deprecated use Engine::Version */
 	public const
